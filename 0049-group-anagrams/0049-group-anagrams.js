e5824f3002ca38a0d1cf510124ef3a429de09d77
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-08 03:50:22 +0530

    Time: 39 ms (31.17%), Space: 69.9 MB (11.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,18 +3,17 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    let hash = new Map();
+    const hash = {};
 
-    for (let i = 0; i < strs.length; i++) {
-        let curr = strs[i];
-        let sorted = curr.split("").sort().join("");
+    for (const str of strs) {
+        const key = str.split("").sort().join("");
 
-        if (!hash.has(sorted)) {
-            hash.set(sorted, []);
+        if (!hash[key]) {
+            hash[key] = [];
         }
 
-        hash.get(sorted).push(curr);
+        hash[key].push(str);
     }
 
-    return [...hash.values()];
+    return Object.values(hash);
 };
