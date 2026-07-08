0c4dfef0f8a5e385f3adfef04aa18bfdcf4670ce
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-09 01:26:27 +0530

    Time: 2 ms (63.59%), Space: 56.6 MB (33.04%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let right = numbers.length - 1;
+    let left = 0;
+
+    while (left < right) {
+        let sum = numbers[left] + numbers[right]
+
+        if (sum === target) {
+            return [left + 1, right + 1]
+        }else if (sum > target) {
+            right--
+        }else {
+            left++
+        }
+    }
+
+    return []
+};
