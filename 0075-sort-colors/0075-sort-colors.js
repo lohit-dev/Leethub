25eeb198ff0e05c7f88cd669c8758fd07a7eebf4
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-09 22:32:48 +0530

    Time: 1 ms (25.81%), Space: 53.8 MB (60.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,19 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let low=0,mid = 0
-    let high = nums.length - 1
+    /// DNF Algorithm
+     let low = 0, mid = 0, high = nums.length - 1;
 
-    for(let i = 0;i<nums.length; i++){
+    while (mid <= high) {
         if (nums[mid] === 0) {
-            [nums[mid], nums[low]]=   [nums[low], nums[mid]]
-            low+=1
-            mid+=1
+            [nums[mid], nums[low]] = [nums[low], nums[mid]];
+            low += 1;
+            mid += 1;
         } else if (nums[mid] === 2) {
-                        [nums[mid], nums[high]]=   [nums[high], nums[mid]]
-                        high -=1 
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high -= 1;
         } else {
-            mid+=1
+            mid += 1;
         }
     }
 };
