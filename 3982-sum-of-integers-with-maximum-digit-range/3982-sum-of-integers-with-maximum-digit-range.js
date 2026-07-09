97147c114cfbd909fc5b7fcd985c5de8bd8b6f51
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-09 19:22:59 +0530

    Time: 9 ms (22.94%), Space: 61.7 MB (14.7%) - LeetHub

--- 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.js
+++ 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.js
@@ -1,22 +1,31 @@
-/**
+ /**
  * @param {number[]} nums
  * @return {number}
  */
 var maxDigitRange = function(nums) {
     let map = new Map()
     let maxRange = 0
+    let res = 0
 
     for(let i = 0; i < nums.length; i++ ) {
         let curr = nums[i].toString().split("").sort().join("")
         let range = Number(curr[curr.length-1]) - Number(curr[0])
 
-        maxRange = Math.max(range, maxRange)
-        if (!map.has(range)) {
-            map.set(range, 0)
+        // maxRange = Math.max(range, maxRange)
+        if(range > maxRange){
+            maxRange = range
+            res = nums[i]
+        } else if (range === maxRange){
+            res += nums[i]
         }
+        
+        // if (!map.has(range)) {
+        //    map.set(range, 0)
+        // }
 
-        map.set(range,map.get(range) + nums[i])
+        // map.set(range,map.get(range) + nums[i])
     }
 
-    return map.get(maxRange)    
+    // return map.get(maxRange)    
+    return res;
 };
