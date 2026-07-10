51567fd220e38224cf87a159c64baf8c775a88ec
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-11 03:40:35 +0530

    Time: 1 ms (56.47%), Space: 57.5 MB (75.35%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
@@ -1,39 +1,38 @@
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
+ *     this.val = (val === undefined ? 0 : val)
+ *     this.next = (next === undefined ? null : next)
  * }
  */
+
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-    let dummy = new ListNode(-1)
-    dummy.next = head
-    
-    let prev = dummy
-    let curr = head
+    let dummy = new ListNode(-1);
+    dummy.next = head;
+
+    let prev = dummy;
+    let curr = head;
 
     while (curr !== null) {
-        // Duplicate run starts
+        // Found the start of a duplicate run
         if (curr.next !== null && curr.val === curr.next.val) {
-            // Skip every node with this value
-            while (curr.next !== null && curr.val === curr.next.val) {
+            const duplicate = curr.val;
+            // Skip every node with this duplicate value
+            while (curr !== null && curr.val === duplicate) {
                 curr = curr.next;
             }
-
-            // Remove the entire duplicate block
-            prev.next = curr.next;
-
+            // Connect the last unique node to the next unique node
+            prev.next = curr;
         } else {
             // Current node is unique
-            prev = prev.next;
+            prev = curr;
+            curr = curr.next;
         }
-
-        curr = curr.next;
     }
 
-    return dummy.next
+    return dummy.next;
 };
