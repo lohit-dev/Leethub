f137b62dc552011442c24f122e8515da3b225d43
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-11 00:39:23 +0530

    Time: 0 ms (100%), Space: 54.8 MB (36.1%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -10,27 +10,48 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
-    let length = 0;
-    let curr = head;
+// var removeNthFromEnd = function(head, n) {
+//     let length = 0;
+//     let curr = head;
 
-    while (curr !== null) {
-        length++;
-        curr = curr.next;
+//     while (curr !== null) {
+//         length++;
+//         curr = curr.next;
+//     }
+//     let index = length - n
+//     let count = 0
+
+//     if (index === 0) return head.next
+
+//     curr = head 
+//     while (curr !== null) {
+//         count+=1
+//         if(count === index) {
+//             curr.next = curr.next.next
+//             break;
+//         }
+//         curr = curr.next
+//     }
+//     return head
+// };
+
+var removeNthFromEnd = function(head, n){
+    if(head === null || head.next === null) return null
+
+    let dummy = new ListNode(-1)
+    dummy.next = head
+    let slow = dummy
+    let fast = dummy
+
+    for (let i = 0; i < n + 1; i++) {
+        fast = fast.next
     }
-    let index = length - n
-    let count = 0
 
-    if (index === 0) return head.next
-
-    curr = head 
-    while (curr !== null) {
-        count+=1
-        if(count === index) {
-            curr.next = curr.next.next
-            break;
-        }
-        curr = curr.next
+    while(fast !== null) {
+        slow = slow.next
+        fast = fast.next
     }
-    return head
-};
+    slow.next = slow.next.next;
+
+   return dummy.next
+}
