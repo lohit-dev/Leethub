1ac1d1794c07a15df461dd4ccba627a2b8bb0bcf
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-10 06:03:14 +0530

    Time: 58 ms (38.04%), Space: 60 MB (16.25%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -10,9 +10,24 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head, visited = new Set()) {
-   if (head === null) return false;
-   if (visited.has(head)) return true;
-   visited.add(head)
-   return hasCycle(head.next, visited)
-};
+// var hasCycle = function(head, visited = new Set()) {
+//    if (head === null) return false;
+//    if (visited.has(head)) return true;
+//    visited.add(head)
+//    return hasCycle(head.next, visited)
+// };
+
+var hasCycle = function (head) {
+    let curr = head
+    let visited = new Set()
+
+    while (curr !== null) {
+        if (visited.has(curr)){
+           return true 
+        }
+        visited.add(curr)
+        curr = curr.next
+    }
+
+    return false
+}
