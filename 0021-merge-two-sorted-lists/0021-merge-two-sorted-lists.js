41df1717a8d92050bbfa1716970097f7bccd204b
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-10 06:34:48 +0530

    Time: 0 ms (100%), Space: 57.6 MB (55.39%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -10,25 +10,37 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+// var mergeTwoLists = function(list1, list2) {
+//    let curr1 = list1
+//    let curr2 = list2
+//    let head = new ListNode(-1);
+//    let tail = head
+
+//    while (curr1 !== null && curr2 !== null)  {
+//     if (curr1.val < curr2.val) {
+//         tail.next = curr1
+//         curr1 = curr1.next
+//     } else {
+//         tail.next = curr2
+//         curr2 = curr2.next
+//     }
+//     tail = tail.next
+//    }
+
+//     if (curr1 !== null) tail.next = curr1
+//     if (curr2 !== null) tail.next = curr2
+
+//    return head.next;
+// };
+
 var mergeTwoLists = function(list1, list2) {
-   let curr1 = list1
-   let curr2 = list2
-   let head = new ListNode(-1);
-   let tail = head
-
-   while (curr1 !== null && curr2 !== null)  {
-    if (curr1.val < curr2.val) {
-        tail.next = curr1
-        curr1 = curr1.next
-    } else {
-        tail.next = curr2
-        curr2 = curr2.next
+    if (list1 === null) return list2
+    if (list2 === null) return list1
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    }else {
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
     }
-    tail = tail.next
-   }
-
-    if (curr1 !== null) tail.next = curr1
-    if (curr2 !== null) tail.next = curr2
-
-   return head.next;
-};
+}
