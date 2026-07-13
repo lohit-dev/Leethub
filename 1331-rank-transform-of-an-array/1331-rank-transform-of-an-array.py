25020ea36e6a7c4bb838506c8c1a80f0887f3b9e
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-13 20:05:20 +0530

    Time: 27 ms (99.22%), Space: 37.5 MB (60.6%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        d = {} 
+        for i,num in enumerate(sorted(set(arr))):
+            d[num] = i+1
+        return [d[num] for num in arr]
+            
+                
+    
