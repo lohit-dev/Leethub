7aabd6d697a9dbcc713ce26a3a9ff323d7103ac5
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-17 23:10:53 +0530

    Time: 102 ms (55.82%), Space: 113.6 MB (5.11%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.js
@@ -0,0 +1,82 @@
+class Node {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.prev = null
+        this.next = null
+    }
+}
+
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+    this.capacity = capacity
+    this.map = new Map()
+    this.head = new Node(0, 0)
+    this.tail = new Node(0, 0)
+
+    this.head.next = this.tail
+    this.tail.next = this.head
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+    if (!this.map.has(key)) return -1
+    const node = this.map.get(key)
+    this._remove(node)
+    this._add(node)
+    return node.value
+
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+    if (this.map.has(key)) {
+        this._remove(this.map.get(key))
+    }
+
+    const newNode = new Node(key, value);
+    this._add(newNode)
+    this.map.set(key, newNode)
+
+    // over our capacity
+    if (this.map.size > this.capacity) {
+        const lru = this.tail.prev
+        this._remove(lru)
+        this.map.delete(lru.key)
+    }
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+LRUCache.prototype._add = function (node) {
+    const first = this.head.next;
+
+    node.prev = this.head;
+    node.next = first;
+
+    first.prev = node;
+    this.head.next = node;
+};
+
+LRUCache.prototype._remove = function (node) {
+    const previous = node.prev;
+    const next = node.next;
+
+    previous.next = next;
+    next.prev = previous;
+};
