fcbb29083974c848163adcdb2c83396e4ca20474
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-18 16:11:39 +0530

    Time: 0 ms (100%), Space: 55.5 MB (54.94%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.js
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.js
@@ -2,14 +2,6 @@
  * @param {number} n
  * @return {number}
  */
-var gcdOfOddEvenSums = function(n) {
-    return gcd(n*n, n*(n+1))
-};
-
-function gcd(x,y) {
-    while (y !== 0){
-        [x,y] = [y,x%y]
-    }
-
-    return x
+var gcdOfOddEvenSums = function (n) {
+    return n
 }
