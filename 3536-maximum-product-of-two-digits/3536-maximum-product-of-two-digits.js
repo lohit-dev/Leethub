9cdf8ce5114447d10767e64c7c5920d094036a97
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-25 19:55:39 +0530

    Time: 1 ms (80.85%), Space: 57.9 MB (6.38%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.js
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.js
@@ -2,12 +2,32 @@
  * @param {number} n
  * @return {number}
  */
-var maxProduct = function (n) {
-    const nums = n
-        .toString()
-        .split("")
-        .map(Number)
-        .sort((a, b) => b - a);
+// var maxProduct = function (n) {
+//     const nums = n
+//         .toString()
+//         .split("")
+//         .map(Number)
+//         .sort((a, b) => b - a);
 
-    return nums[0] * nums[1];;
+//     return nums[0] * nums[1];;
+// }
+
+var maxProduct = function (n) {
+    let largestDigit = 0;
+    let secondLargestDigit = 0;
+
+    while (n > 0) {
+        const currentDigit = n % 10;
+
+        if (currentDigit >= largestDigit) {
+            secondLargestDigit = largestDigit;
+            largestDigit = currentDigit;
+        } else if (currentDigit > secondLargestDigit) {
+            secondLargestDigit = currentDigit;
+        }
+
+        n = Math.floor(n / 10);
+    }
+
+    return largestDigit * secondLargestDigit;
 }
