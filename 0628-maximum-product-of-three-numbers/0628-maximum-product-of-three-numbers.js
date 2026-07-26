cb6253bf52eeb8df76c29bd79a2991adfee6e20c
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-26 16:52:07 +0530

    Time: 34 ms (69.64%), Space: 58.8 MB (63.84%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -2,36 +2,49 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var maximumProduct = function (nums) {
+//     let max1 = -Infinity;
+//     let max2 = -Infinity;
+//     let max3 = -Infinity;
+
+//     let min1 = Infinity;
+//     let min2 = Infinity;
+
+//     for (const num of nums) {
+//         if (num >= max1) {
+//             max3 = max2;
+//             max2 = max1;
+//             max1 = num;
+//         } else if (num >= max2) {
+//             max3 = max2;
+//             max2 = num;
+//         } else if (num > max3) {
+//             max3 = num;
+//         }
+
+//         if (num <= min1) {
+//             min2 = min1;
+//             min1 = num;
+//         } else if (num < min2) {
+//             min2 = num;
+//         }
+//     }
+
+//     return Math.max(
+//         max1 * max2 * max3,
+//         max1 * min1 * min2
+//     );
+// };
+
 var maximumProduct = function (nums) {
-    let max1 = -Infinity;
-    let max2 = -Infinity;
-    let max3 = -Infinity;
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
 
-    let min1 = Infinity;
-    let min2 = Infinity;
+    const largestThree =
+        nums[n - 1] * nums[n - 2] * nums[n - 3];
 
-    for (const num of nums) {
-        if (num >= max1) {
-            max3 = max2;
-            max2 = max1;
-            max1 = num;
-        } else if (num >= max2) {
-            max3 = max2;
-            max2 = num;
-        } else if (num > max3) {
-            max3 = num;
-        }
+    const twoSmallestAndLargest =
+        nums[0] * nums[1] * nums[n - 1];
 
-        if (num <= min1) {
-            min2 = min1;
-            min1 = num;
-        } else if (num < min2) {
-            min2 = num;
-        }
-    }
-
-    return Math.max(
-        max1 * max2 * max3,
-        max1 * min1 * min2
-    );
-};
+    return Math.max(largestThree, twoSmallestAndLargest);
+}
