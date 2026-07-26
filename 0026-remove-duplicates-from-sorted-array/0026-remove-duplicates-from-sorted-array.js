4b3c0dc3321efe3a27c5856a83bdd3bf60d3a8b9
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-27 02:27:46 +0530

    Time: 0 ms (100%), Space: 58.2 MB (40.51%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    left = 1
+    let left = 1
 
     for(let i = 0; i < nums.length; i++){
         if (nums[i] !== nums[left - 1]) {
