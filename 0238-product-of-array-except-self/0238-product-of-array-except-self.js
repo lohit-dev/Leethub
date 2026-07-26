23b02f21aaa14a3c53ff862df3dd4d525abb3267
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-27 02:02:07 +0530

    Time: 5 ms (77.59%), Space: 69.3 MB (79.66%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,19 +2,19 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
-  let result = new Array(nums.length).fill(1);
+var productExceptSelf = function (nums) {
+    let result = new Array(nums.length).fill(1);
 
-  result[0] = 1;
-  for (let i = 1; i < nums.length; i++) {
-    result[i] = result[i - 1] * nums[i - 1];
-  }
+    result[0] = 1;
+    for (let i = 1; i < nums.length; i++) {
+        result[i] = result[i - 1] * nums[i - 1];
+    }
 
-  let suffix = 1;
-  for (let i = nums.length - 1; i >= 0; i--) {
-    result[i] = result[i] * suffix;
-    suffix = suffix * nums[i];
-  }
+    let suffix = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] = result[i] * suffix;
+        suffix = suffix * nums[i];
+    }
 
-  return result;t
+    return result; t
 };
