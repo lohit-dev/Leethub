ba103e50db14f76b49e45d6e532915e761b73d89
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-27 02:20:58 +0530

    Time: 21 ms (43.25%), Space: 76.7 MB (24.98%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    const seen = new Set(nums);
+const result = [];
+
+for (let i = 1; i <= nums.length; i++) {
+    if (!seen.has(i)) {
+        result.push(i);
+    }
+}
+
+return result;
+};
