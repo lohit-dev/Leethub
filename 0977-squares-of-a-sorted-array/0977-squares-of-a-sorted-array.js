6ab2981d516d5b5db8535bcecf046bb859b0924b
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-07-30 06:01:58 +0530

    Time: 5 ms (59.6%), Space: 60.3 MB (97.61%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -2,10 +2,29 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        nums[i] = nums[i] * nums[i];
+// var sortedSquares = function (nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         nums[i] = nums[i] * nums[i];
+//     }
+//     nums.sort((a, b) => a - b);
+//     return nums;
+// };
+
+var sortedSquares = function (nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    let result = [];
+    while (left <= right) {
+        if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+            result.push(nums[left] * nums[left]);
+            left++;
+        } else {
+            result.push(nums[right] * nums[right]);
+            right--;
+        }
     }
-    nums.sort((a, b) => a - b);
-    return nums;
-};
+    // console.log(result);
+
+    return result.reverse();
+}
