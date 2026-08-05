cb4a8120e1d4db5b7bfa533593308ac453922cb4
lohit <133247459+lohit-dev@users.noreply.github.com>
2026-08-05 06:13:03 +0530

    Time: 3 ms (64.62%), Space: 59 MB (26.92%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findMissingElements = function (nums) {
+    const hashset = new Set(nums);
+    let result = []
+    let smallest = nums[0]
+    let largest = nums[0]
+
+    for (const num of nums) {
+        smallest = Math.min(smallest, num)
+        largest = Math.max(largest, num)
+    }
+
+    for (let i = smallest; i < largest; i++) {
+        if (!hashset.has(i)) {
+            result.push(i)
+        }
+    }
+
+    return result
+};
